34f4225e200a0ad2e3fc0be0a7683ea4796de869
lrylander <linnearylander@gmail.com>
2018-01-15 22:48:26 -0500

    Revert "Fixed lag by changing water and grass postitions to fixed"
    This reverts commit 72adbb513081bddb1b9c93016ab4c0384d63515e.

--- js/index.js
+++ js/index.js
@@ -29,19 +29,3 @@ function resize() {
   
   dummy_x = last_pane.getBoundingClientRect().left+window.scrollY;
 }
-
-function zoomIn()
-{
-    var Page = document.getElementById('Body');
-    var zoom = parseInt(Page.style.zoom) + 10 +'%'
-    Page.style.zoom = zoom;
-    return false;
-}
- 
-function zoomOut()
-{
-    var Page = document.getElementById('Body');
-    var zoom = parseInt(Page.style.zoom) - 10 +'%'
-    Page.style.zoom = zoom;
-    return false;
-}
