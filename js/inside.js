e9aec07a9fd38a77d61bcb6a714dd4afefee078d
lrylander <linnearylander@gmail.com>
2018-01-20 23:00:17 -0500

    added words and graphics

--- js/inside.js
+++ js/inside.js
@@ -106,3 +106,8 @@ TweenLite.set(contain,{perspective:300});
 TweenMax.staggerTo('.R-D,.R-T',1,{rotationX:180,repeat:-1,ease:Linear.easeNone,delay:-C/2},1/(C/2));
 TweenMax.to('.R-D,.R-T',40,{rotation:360,repeat:-1,ease:Linear.easeNone});
 TweenMax.to('#core',1.5,{scale:0.95,repeat:-1,yoyo:true,ease:Sine.easeInOut});
+
+
+
+
+
