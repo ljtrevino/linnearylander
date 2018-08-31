08217156629972f06a615c496189a0bb699fcbb8
lrylander <linnearylander@gmail.com>
2018-08-30 20:03:36 -0400

    redirect test

--- test/js/index.js
+++ test/js/index.js
@@ -1,15 +1,3 @@
-
-function mobile_detect(mobile,tablet,mobile_redirect,tablet_redirect) {
- var ismobile = (/iphone|ipod|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(navigator.userAgent.toLowerCase()));
- var istablet = (/ipad|android 3|sch-i800|playbook|tablet|kindle|gt-p1000|sgh-t849|shw-m180s|a510|a511|a100|dell streak|silk/i.test(navigator.userAgent.toLowerCase()));
-
- if (ismobile && mobile==true) {
- window.location.href = 'https://lrylander.github.io/linnearylander/mobile/'
- } else if (istablet && tablet==true) {
- window.location.href = 'https://lrylander.github.io/linnearylander/mobile/'
- }
-}
-
 var page = document.getElementById('page');
 var last_pane = page.getElementsByClassName('pane');
 last_pane = last_pane[last_pane.length-1];
