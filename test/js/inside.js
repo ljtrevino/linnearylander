257025f6796e13b992fb4693447400b768c209da
lrylander <linnearylander@gmail.com>
2018-09-01 09:59:42 -0400

    progress bar updates

--- test/js/inside.js
+++ test/js/inside.js
@@ -75,19 +75,6 @@ scrollConverter.activate(function (offset) {
 });
 
 
-
-//CROCHET
-var R = 200 , dur = 15; 
-
-var tl = new TimelineLite()
-.staggerTo(".small_cookie", dur ,{
-bezier:{curviness:1.5,values:[{x:0,y:0},{x:R/2,y:R/2},{x:0,y:R},{x:-R/2,y:R/2},{x:0,y:0}]}
-,rotation:-720,repeat:-1,ease:Power0.easeNone},dur/20)
-.to(".main_cookie", dur ,{rotation:-0,ease:Power0.easeNone,repeat:-1},0)
-.time(dur);
-
-
-
 //logic
 
 
