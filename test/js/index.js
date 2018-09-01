308de970c47a3cb883837dd1edc4173c340b4bf6
lrylander <linnearylander@gmail.com>
2018-08-31 20:44:27 -0400

    local storage

--- test/js/index.js
+++ test/js/index.js
@@ -92,7 +92,11 @@ function sky(){
     window.location = 'https://lrylander.github.io/linnearylander/test/sky/index.html'
 }
 
-if (screen.width <= 699) {
-document.location = "https://lrylander.github.io/linnearylander/test/mobile";
+//if (screen.width <= 699) {
+//document.location = "https://lrylander.github.io/linnearylander/test/mobile";
+//}
+function checkPrevious(){
+if (sessionStorage["previousPage"] == "sky"){
+    education();
 }
-
+}
